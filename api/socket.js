ae13f617779c21fdf3dd5b9f0698a696f723ef02
Prakruti panchal <prakrutipanchal2005@gmail.com>
2025-03-30 18:45:20 +0530

    Final updated code

--- api/socket.js
+++ api/socket.js
@@ -1,18 +1,31 @@
 import { Server } from 'socket.io';
 
+// Workaround for Vercel Serverless cold starts
+let io;
+
 export default function handler(req, res) {
-  if (!res.socket.server.io) {
-    console.log('Starting Socket.IO...');
-    const io = new Server(res.socket.server, {
+  if (!io) {
+    console.log('Initializing Socket.IO');
+    const httpServer = res.socket.server;
+    
+    io = new Server(httpServer, {
       path: '/socket.io/',
-      cors: { origin: '*' }
+      addTrailingSlash: false,
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+      }
     });
 
     io.on('connection', (socket) => {
+      console.log('New connection:', socket.id);
       socket.emit('welcome', 'Connected to Vercel!');
     });
 
-    res.socket.server.io = io;
+    // Attach to Vercel's server
+    httpServer.io = io;
   }
+  
+  // End HTTP request (keep WebSocket open)
   res.end();
 }
